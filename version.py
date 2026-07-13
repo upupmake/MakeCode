47df94a83dd58cf82a63646cb7fec9bb10b8bfc7
Ke Ma <81823756+upupmake@users.noreply.github.com>
2026-07-13 16:29:14 +0800

    Update CURRENT_VERSION to 5.0.3

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-CURRENT_VERSION = "5.0.1"
+CURRENT_VERSION = "5.0.3"
 
 GITHUB_RELEASE_BASE_URL = "https://github.com/upupmake/MakeCode/releases/latest/download"
 VERSION_CHECK_URL = f"{GITHUB_RELEASE_BASE_URL}/version.json"
