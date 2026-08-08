4e6b0c38c8ddb181ed550f844c0a960111b70031
make08 <make08@baidu.com>
2026-08-08 19:08:22 +0800

    release: v6.2.3

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-CURRENT_VERSION = "6.2.2"
+CURRENT_VERSION = "6.2.3"
 
 GITHUB_RELEASE_BASE_URL = "https://github.com/upupmake/MakeCode/releases/latest/download"
 VERSION_CHECK_URL = f"{GITHUB_RELEASE_BASE_URL}/version.json"
